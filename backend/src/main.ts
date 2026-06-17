4852c087568a8d08ff40e13131e55f5784d5505e
Alize_Software_Services <alizesoftwareservicesllp@gmail.com>
2026-06-17 09:46:51 +0530

    Fix database connection URL for Render deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
-process.env.DATABASE_URL = "postgresql://admin:adminpassword@localhost:5432/job_portal?schema=public";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);

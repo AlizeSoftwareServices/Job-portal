fb03845f0fce153d25592edfda0c766b8efc7cf0
Alize_Software_Services <alizesoftwareservicesllp@gmail.com>
2026-07-08 14:20:31 +0530

    Remove one-off debug scripts

--- test-dashboard.js
+++
@@ -1,8 +0,0 @@
-const https = require('https');
-
-https.get('https://skyo-backend.onrender.com/admin/dashboard-data', (res) => {
-  console.log('STATUS:', res.statusCode);
-  let data = '';
-  res.on('data', chunk => data += chunk);
-  res.on('end', () => console.log('BODY:', data));
-}).on('error', err => console.log(err));
